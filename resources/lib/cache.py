9798e3b09b0c9977503d82faff4d71ae6b5abe9d
alex <766f6964+github@gmail.com>
2025-07-02 12:01:14 +0200

    moved clear cache path to external lib

--- resources/lib/cache.py
+++ resources/lib/cache.py
@@ -4,6 +4,15 @@ from pathlib import Path
 import tempfile
 import os
 
+def clear_cache_path(directory_path):
+    for name in os.listdir(directory_path):
+        path = os.path.join(directory_path, name)
+        if os.path.isfile(path) or os.path.islink(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            clear_cache_path(path)
+            os.rmdir(path)
+
 def get_cache_path(settings_root_path = None):
     if settings_root_path is not None and os.path.exists(settings_root_path):
         CACHE_BASE_PATH = str(Path(settings_root_path))
